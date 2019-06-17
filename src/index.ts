85f9b0ececbe979cc65c17f570542db7ffb158a8
Tomasz Martyński <tomek@martynski.pl>
2019-06-17 20:07:13 +0200

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("eyyeye");
+import { spawn } from "child_process";
+import { config } from "dotenv";
+import * as path from "path";
+
+const main = async () => {
+  config({ path: path.join(process.cwd(), ".env") });
+
+  const { DOCKER_COMPOSE_OVERRIDES } = process.env;
+
+  if (
+    typeof DOCKER_COMPOSE_OVERRIDES !== "string" ||
+    !DOCKER_COMPOSE_OVERRIDES
+  ) {
+    console.error(`'DOCKER_COMPOSE_OVERRIDES' is required on env!`);
+    return process.exit(1);
+  }
+
+  const dockerComposes = DOCKER_COMPOSE_OVERRIDES.split(",");
+
+  const argvTopass = [...process.argv];
+  argvTopass.shift();
+  argvTopass.shift();
+
+  const options = [...dockerComposes.map(file => `-f ${file}`), ...argvTopass];
+
+  const childProcess = spawn("docker-compose", options, {
+    stdio: [process.stdin, process.stdout, process.stderr]
+  });
+};
+
+main();
