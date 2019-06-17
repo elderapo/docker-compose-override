7991382622cb7d6a5c0417b4a4c62c4b0a854127
Tomasz Martyński <tomek@martynski.pl>
2019-06-17 21:25:58 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,25 +1,24 @@
 # !/usr/bin/env bash
 
 TMP=$(mktemp -d)
-echo "Cloning repo..." 
+echo "Cloning repo..."
 
 git clone --quiet --depth 1 git@github.com:elderapo/docker-compose-override.git $TMP
-{ # try
-    echo "Installing deps using yarn..."
-    yarn install --silent --no-progress --cwd ${TMP} --frozen-lockfile
-} || { # catch
-    echo "Installation using yarn failed..."
-    echo "Installing deps using npm..."
-    npm install --prefix ${TMP}
-}
+
+echo "Installing deps (yarn)..."
+yarn --cwd "${TMP}" install --silent --no-progress --frozen-lockfile
+
+echo "Building library (yarn)..."
+yarn --cwd "${TMP}" run build >/dev/null
+
 
 echo "Preparing executable..."
-cp build/index.js build/tmp
-(printf "#!/usr/bin/env node\n\n"; cat build/tmp) > build/dco
-chmod +x build/dco
+cp -r ${TMP}/build/index.js ${TMP}/build/tmp
+(printf "#!/usr/bin/env node\n\n"; cat ${TMP}/build/tmp) > ${TMP}/build/dco
+chmod +x ${TMP}/build/dco
 
 TARGET_DIR=/usr/local/bin
 
-sudo cp -r build/dco $TARGET_DIR
+sudo cp -r ${TMP}/build/dco $TARGET_DIR
 sudo ln -sf $TARGET_DIR/dco $TARGET_DIR/docker-compose-override
 echo 'dco (docker-compose-override) has been successfully installed 👍!'
